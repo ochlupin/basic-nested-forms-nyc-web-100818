b8291a1395cd55594bbaa242227947449edc4d3c
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-11-08 00:15:34 -0500

    Done.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,9 +1,12 @@
 class PeopleController < ApplicationController
   def new
     @person = Person.new
+    # stubs
+    @person.addresses.build(address_type: 'work')
+    @person.addresses.build(address_type: 'home')
   end
 
-  def create    
+  def create
     Person.create(person_params)
     redirect_to people_path
   end
@@ -15,6 +18,14 @@ class PeopleController < ApplicationController
   private
 
   def person_params
-    params.require(:person).permit(:name)
+    params.require(:person).permit(:name,
+                                   addresses_attributes: %i[
+                                     street_address_1
+                                     street_address_2
+                                     city
+                                     state
+                                     zipcode
+                                     address_type
+                                   ])
   end
 end
